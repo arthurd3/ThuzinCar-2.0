8832d0e561ee494765341b2d984ce5b75b459df0
arthurd3 <arthur_camposl@yahoo.com>
2025-04-13 00:21:19 -0300

    feat : add project structure

---
+++ thuzincar/src/main/java/com/arthurd/thuzincar/ThuzincarApplication.java
@@ -0,0 +1,13 @@
+package com.arthurd.thuzincar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThuzincarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThuzincarApplication.class, args);
+    }
+
+}
---
+++ thuzincar/src/test/java/com/arthurd/thuzincar/ThuzincarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurd.thuzincar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThuzincarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
